aa1261468823db5bc9a05384d73054d34f8dcf00
mw <willia32@hotmail.com>
2017-09-11 15:29:21 -0400

    Complete TODO 1-2 with fragment

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,9 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        // [...]/job?id=X
+        Job selectedJob = jobData.findById(id);
+        model.addAttribute("selectedJob", selectedJob);
 
         return "job-detail";
     }
